1a28e14c323e90cab007210ca14ae78a3138c5ed
Graeme Williams <graeme.m.williams@gmail.com>
2024-12-03 13:27:33 -0500

    Add Day3.Part2 Solution

--- AdventOfCode2024/Day3/DayThree.cpp
+++ AdventOfCode2024/Day3/DayThree.cpp
@@ -13,7 +13,7 @@ void DayThree::CrunchPartOne(string& input)
     
     // Matching on mul(<1-3 digit number>,<1-3 digit number>)
     std::smatch match;
-    std::regex exp("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
+    std::regex exp(matchExpression1);
     
     // Define iterator to loop over input
     int subMatches[] = { 1, 2 };
@@ -39,5 +39,47 @@ void DayThree::CrunchPartOne(string& input)
 
 void DayThree::CrunchPartTwo(string& input)
 {
-    std::cout << input;
+    int totalProduct = 0;
+    
+    // Matching on mul(<1-3 digit number>,<1-3 digit number>)
+    std::smatch match;
+    std::regex exp(matchExpression2);
+    
+    // Define iterator to loop over input
+    int subMatches[] = { 0, 1, 2 }; // triplet entries (whole match, sub1, sub2)
+    std::regex_token_iterator<std::string::iterator> matchIterator (input.begin(), input.end(), exp, subMatches);
+    std::regex_token_iterator<std::string::iterator> rend;     // default constructor = end-of-sequence:
+    
+    // New logic for skips
+    int subMatchStep = 0; // triplets, as above
+    bool doEnabled = true;
+    bool skipNextSubMatches = false;
+    int firstEntry = 0; // Memory for storing first entry in mul(x,y)
+    while (matchIterator!=rend)
+    {
+        std::string subMatchEntry = *matchIterator++;
+        
+        if (subMatchStep == 0)
+        {
+            // Handling for toggling disable / enable on mul(x,y)
+            if (subMatchEntry.find(mulDisableCommand) != std::string::npos) { doEnabled = false; skipNextSubMatches = true; }
+            if (subMatchEntry.find(mulEnableCommand) != std::string::npos) { doEnabled = true; skipNextSubMatches = true; }
+        }
+        else
+        {
+            if (doEnabled && !skipNextSubMatches)
+            {
+                if (subMatchStep == 1) { firstEntry = stoi(subMatchEntry); }
+                else if (subMatchStep == 2) { totalProduct += (stoi(subMatchEntry) * firstEntry); }
+            }
+        }
+        
+        // Increment sub-match -- comes in triplets based on subMatches, as above
+        if (subMatchStep == 2) { subMatchStep = 0; skipNextSubMatches = false; }
+        else { subMatchStep++; }
+    }
+    
+    //Output
+    std::cout << "Total product is:" << std::endl;
+    std::cout << totalProduct << std::endl;
 }
