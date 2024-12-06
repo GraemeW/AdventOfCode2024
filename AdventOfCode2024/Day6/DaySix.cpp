3a65d0bf6059bc2146aab8ca589536f933764111
Graeme Williams <graeme.m.williams@gmail.com>
2024-12-06 09:51:13 -0500

    Add Day5.Part1 Solution

--- AdventOfCode2024/Day6/DaySix.cpp
+++ AdventOfCode2024/Day6/DaySix.cpp
@@ -4,11 +4,123 @@
 //
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
 #include "DaySix.hpp"
+
+int DaySix::ConvertInputToFacilityEntry(char const& inputCharacter)
+{
+    switch(inputCharacter) {
+        case inputOpenSpace: return facilityOpenSpace;
+        case inputBlockedSpace: return facilityBlockedSpace;
+        case inputGuard: return facilityGuard;
+    }
+    return -1;
+}
+
+void DaySix::ReadInputToFacilityLayout(string const& input, std::vector<std::vector<int>>& facilityLayout, std::vector<int>& guardStartPosition)
+{
+    int facilityWidth = -1;
+    
+    std::istringstream lineStream(input);
+    int row = 0;
+    for (std::string line; std::getline(lineStream, line); ) {
+        // Avoid dynamic allocation, pre-reserve the space
+        if (facilityWidth == -1) { facilityWidth = (int)line.size(); }
+        std::vector<int> facilityRow;
+        facilityRow.reserve(facilityWidth);
+        
+        int col = 0;
+        for(char& inputEntry : line) {
+            int facilityEntry = ConvertInputToFacilityEntry(inputEntry);
+            if (facilityEntry == facilityGuard) { guardStartPosition = {row, col}; }
+            facilityRow.push_back(facilityEntry);
+            col++;
+        }
+        facilityLayout.push_back(facilityRow);
+        row++;
+    }
+}
+
+std::vector<int> DaySix::GetTranslationDirection(DaySix::Direction direction)
+{
+    switch(direction) {
+        case DaySix::Direction::North: return {-1, 0};
+        case DaySix::Direction::East: return {0, 1};
+        case DaySix::Direction::South: return {1, 0};
+        case DaySix::Direction::West: return {0, -1};
+    }
+    return {0, 0};
+}
+
+DaySix::Direction DaySix::GetNextDirection(DaySix::Direction direction)
+{
+    switch (direction) {
+        case DaySix::Direction::North: return DaySix::Direction::East;
+        case DaySix::Direction::East: return DaySix::Direction::South;
+        case DaySix::Direction::South: return DaySix::Direction::West;
+        case DaySix::Direction::West: return DaySix::Direction::North;
+    }
+}
+
+bool DaySix::IsGuardInBounds(int guardRow, int guardCol)
+{
+    if (guardRow < facilityRowBounds[0] || guardRow > facilityRowBounds[1]) { return false; }
+    if (guardCol < facilityColBounds[0] || guardCol > facilityColBounds[1]) {return false; }
+    return true;
+}
+
+bool DaySix::IsPositionInHistory(int guardRow, int guardCol)
+{
+    string guardHistoryEntry = std::to_string(guardRow) + historyDelimiter + std::to_string(guardCol);
+    bool isPositionInHistory =(guardPositionHistory.find(guardHistoryEntry) != guardPositionHistory.end());
+    guardPositionHistory[guardHistoryEntry]++;
+    return isPositionInHistory;
+}
 
 void DaySix::CrunchPartOne(string& input)
 {
-    std::cout << input;
+    std::vector<std::vector<int>> facilityLayout;
+    std::vector<int> guardStartPosition = {0, 0};
+    ReadInputToFacilityLayout(input, facilityLayout, guardStartPosition);
+    
+    // N.B. Bounds set exit criteria (i.e. when guard traverses off of facility space)
+    facilityRowBounds[1] = (int)facilityLayout.size();
+    facilityColBounds[1] = (int)facilityLayout[0].size();
+    
+    // Traverse through guard path
+    int totalGuardPositions = 1; // count the first position
+    
+    Direction direction = guardStartDirection;
+    int guardRow = guardStartPosition[0];
+    int guardCol = guardStartPosition[1];
+    
+    while (IsGuardInBounds(guardRow, guardCol))
+    {
+        // Check for obstacles
+        std::vector<int> translationDirection = GetTranslationDirection(direction);
+        int tentativeGuardRow = guardRow + stepSize * translationDirection[0];
+        int tentativeGuardCol = guardCol + stepSize * translationDirection[1];
+        if (!IsGuardInBounds(tentativeGuardRow, tentativeGuardCol)) { break; } // Break early, otherwise addressing below fails
+        
+        if (facilityLayout[tentativeGuardRow][tentativeGuardCol] == facilityBlockedSpace)
+        {
+            direction = GetNextDirection(direction);
+            continue;
+        }
+        
+        // Step
+        guardRow = tentativeGuardRow;
+        guardCol = tentativeGuardCol;
+        
+        // Check for history --
+        if (!IsPositionInHistory(guardRow, guardCol)) { totalGuardPositions++; }
+    }
+    
+    //Output
+    std::cout << "Total guard positions is:" << std::endl;
+    std::cout << totalGuardPositions << std::endl;
 }
 
 void DaySix::CrunchPartTwo(string& input)
--- AdventOfCode2024/Day6/DaySix.hpp
+++ AdventOfCode2024/Day6/DaySix.hpp
@@ -5,6 +5,8 @@
 #pragma once
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <map>
 #include "../Day.h"
 
 #ifndef DaySix_hpp
@@ -12,7 +14,32 @@
 class DaySix : public Day
 {
 private:
+    // Tunables
     string inputPath = "Resources/DaySixInput.txt";
+    enum Direction { North, East, South, West };
+    
+    static const char inputOpenSpace = '.';
+    static const char inputBlockedSpace = '#';
+    static const char inputGuard = '^';
+    int facilityOpenSpace = 0;
+    int facilityBlockedSpace = 1;
+    int facilityGuard = 2;
+    int stepSize = 1;
+    std::string historyDelimiter = "-";
+    Direction guardStartDirection = Direction::North;
+    
+    // State
+    std::map<string, int> guardPositionHistory;
+    std::vector<int> facilityRowBounds = {0, 0};
+    std::vector<int> facilityColBounds = {0, 0};
+    
+    // Methods
+    void ReadInputToFacilityLayout(string const& input, std::vector<std::vector<int>>& facilityLayout, std::vector<int>& guardStartPosition);
+    int ConvertInputToFacilityEntry(char const& inputCharacter);
+    std::vector<int> GetTranslationDirection(Direction direction);
+    Direction GetNextDirection(Direction direction);
+    bool IsGuardInBounds(int guardRow, int guardCol);
+    bool IsPositionInHistory(int guardRow, int guardCol);
 
 public:
     // Methods
