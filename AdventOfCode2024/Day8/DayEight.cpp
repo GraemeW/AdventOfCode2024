982b8563be8386f61dc0804cc396ac426cf03267
Graeme Williams <graeme.m.williams@gmail.com>
2024-12-08 08:26:48 -0500

    Day8:  Define problem statement and input data

---
+++ AdventOfCode2024/Day8/DayEight.cpp
@@ -0,0 +1,18 @@
+//
+//  DayEight.cpp
+//  AdventOfCode2024
+//
+
+#include <string>
+#include <iostream>
+#include "DayEight.hpp"
+
+void DayEight::CrunchPartOne(string& input)
+{
+    std::cout << input;
+}
+
+void DayEight::CrunchPartTwo(string& input)
+{
+    std::cout << input;
+}
--- AdventOfCode2024/Days.h
+++ AdventOfCode2024/Days.h
@@ -15,4 +15,5 @@
 #include "Day5/DayFive.hpp"
 #include "Day6/DaySix.hpp"
 #include "Day7/DaySeven.hpp"
+#include "Day8/DayEight.hpp"
 #endif /* Days_h */
